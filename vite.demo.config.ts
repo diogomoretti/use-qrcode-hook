b4ae288626a7979e9463949f4698e23a0f5b09ae
Diogo Moretti <diogo.pearljam@gmail.com>
2024-12-06 09:19:11 -0300

    pages: fix path to assets

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './src',
+  base: '',
   build: {
     outDir: 'demo',
+    assetsDir: 'assets',
   },
 });
